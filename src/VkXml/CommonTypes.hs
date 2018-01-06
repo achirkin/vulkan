fc811639f5b1b88ebab2ec3a01533f03f3c4f2dd
achirkin <chirkin@arch.ethz.ch>
2018-01-06 10:10:14 +0100

    parsed enums successfully

--- src/VkXml/CommonTypes.hs
+++ src/VkXml/CommonTypes.hs
@@ -1,15 +1,24 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE OverloadedStrings          #-}
 module VkXml.CommonTypes
   ( VkName (..)
   , VkEnumValueName (..)
   , VkTypeName (..), VkMemberName (..)
+  , Sections (..), parseSections
   ) where
 
+import           Control.Monad.State.Class
+import           Control.Monad.Trans.Class
 import           Data.String                (IsString)
 import           Data.Text                  (Text)
+import           Data.Conduit
+import           Data.Conduit.Lift
+import           Data.XML.Types
+import           Text.XML.Stream.Parse
 
--- newtype VkEnumName = VkEnumName { unVkEnumName :: Text }
---   deriving (Eq, Ord, Show, Read, IsString)
+import           VkXml.Parser
+
 
 newtype VkEnumValueName = VkEnumValueName { unVkEnumValueName :: Text }
   deriving (Eq, Ord, Show, Read, IsString)
@@ -25,3 +34,39 @@ newtype VkMemberName = VkMemberName { unVkMemberName :: Text }
 -- | Some identifier
 newtype VkName = VkName { unVkName :: Text }
   deriving (Eq, Ord, Show, Read, IsString)
+
+-- | Parse a list of elemenents interspersed with comments,
+--   that overwise would be a homogenous list of xml tag.
+data Sections a
+  = Sections
+  { items :: [a]
+    -- ^ List of items in original order from xml file
+  , comments :: [(Int, Text)]
+    -- ^ Comments and their indices in between items.
+    --   e.g. (0,"abc") means comment inserted before anything else.
+    --   e.g. (length (items xs), "abc") means comment inserted after a.e. .
+  } deriving Show
+
+-- | Parse elements and comments in between them.
+parseSections :: VkXmlParser m
+              => Sink Event m (Maybe a) -- ^ how to parse elements
+              -> Sink Event m (Sections a)
+parseSections parseElem = evalStateC (0::Int) parseIt
+  where
+    prepComment c ~secs@(Sections _ cs) = secs{ comments = c:cs}
+    prepItem    e ~secs@(Sections es _) = secs{ items    = e:es}
+    parseIt = do
+      mnewcomment <- tagIgnoreAttrs "comment" content
+      me <- transPipe lift parseElem
+      case (mnewcomment, me) of
+        (Nothing, Nothing) -> pure $ Sections [] []
+        (Just comment, Just e) -> do
+          i <- get
+          modify' (+1)
+          prepComment (i,comment) . prepItem e <$> parseIt
+        (Just comment, Nothing) -> do
+          i <- get
+          prepComment (i,comment) <$> parseIt
+        (Nothing, Just e) -> do
+          modify' (+1)
+          prepItem e <$> parseIt
--- src/VkXml/Sections/Enums.hs
+++ src/VkXml/Sections/Enums.hs
@@ -25,7 +25,6 @@ import           Text.XML.Stream.Parse
 import           VkXml.CommonTypes
 import           VkXml.Parser
 
-
 -- * Types
 
 
@@ -33,17 +32,17 @@ data VkEnums
   = VkEnums
     { name        :: VkTypeName
     , comment     :: Maybe Text
-    , memberEnums :: [VkEnumValue]
+    , memberEnums :: Sections VkEnumValue
     }
   | VkBitmasks
     { name        :: VkTypeName
     , comment     :: Maybe Text
-    , memberMasks :: [VkBitmaskValue]
+    , memberMasks :: Sections VkBitmaskValue
     }
   | VkConstants
     { name         :: VkTypeName
     , comment      :: Maybe Text
-    , memberConsts :: [VkConstant]
+    , memberConsts :: Sections VkConstant
     }
   deriving Show
 
@@ -51,15 +50,20 @@ data VkEnumValue
   = VkEnumValue
     { name    :: VkEnumValueName
     , comment :: Maybe Text
-    , value   :: Word
+    , value   :: Int
     }
   deriving Show
 
 data VkBitmaskValue
-  = VkBitmaskValue
+  = VkBitmaskBitpos
     { name    :: VkEnumValueName
     , comment :: Maybe Text
     , bitpos  :: Word
+    }
+  | VkBitmaskValue
+    { name    :: VkEnumValueName
+    , comment :: Maybe Text
+    , value   :: Int
     }
   deriving Show
 
@@ -80,22 +84,27 @@ data VkConstant
 --
 --   * If tag name does not match, return events upstream as leftovers
 --   * If failed to parse tag "enums", throw an exception
+--
+--   Note: enum "VkResult" also has a member "unused",
+--    I ignore this member, because it does not seem to be useful for codegen.
 parseEnums :: VkXmlParser m
            => Sink Event m (Maybe VkEnums)
 parseEnums = parseTagForceAttrs "enums" parseVkEnumsAttrs $
   \case
     VkEnums n c _
       -> fmap (VkEnums n c)
-      . many $ parseTagForceAttrs "enum" parseVkEnumValueAttrs pure
+      . parseSections $ ignoreEmptyTag "unused"
+                      >> parseTagForceAttrs "enum" parseVkEnumValueAttrs pure
 
     VkBitmasks n c _
       -> fmap (VkBitmasks n c)
-      . many $ parseTagForceAttrs "enum" parseVkBitmaskValueAttrs pure
+      . parseSections $ ignoreEmptyTag "unused"
+                      >> parseTagForceAttrs "enum" parseVkBitmaskValueAttrs pure
 
     VkConstants n c _
       -> fmap (VkConstants n c)
-      . many $ parseTagForceAttrs "enum" parseVkConstantAttrs pure
-
+      . parseSections $ ignoreEmptyTag "unused"
+                      >> parseTagForceAttrs "enum" parseVkConstantAttrs pure
 
 
 
@@ -106,9 +115,9 @@ parseVkEnumsAttrs = do
   mc <- lift $ attr "comment"
   met <- lift $ attr "type"
   case met of
-    Nothing        -> pure $ VkConstants n mc []
-    Just "enum"    -> pure $ VkEnums n mc []
-    Just "bitmask" -> pure $ VkEnums n mc []
+    Nothing        -> pure . VkConstants n mc $ Sections [] []
+    Just "enum"    -> pure . VkEnums n mc $ Sections [] []
+    Just "bitmask" -> pure . VkBitmasks n mc $ Sections [] []
     Just tt        -> parseFailed
                     $ "uknown enums type " <> show tt
 
@@ -116,7 +125,7 @@ parseVkEnumValueAttrs :: ReaderT ParseLoc AttrParser VkEnumValue
 parseVkEnumValueAttrs = do
   n <- VkEnumValueName <$> forceAttr "name"
   mc <- lift $ attr "comment"
-  ev <- T.decimal <$> forceAttr "value"
+  ev <- T.signed T.decimal <$> forceAttr "value"
   case ev of
     Left err -> parseFailed
               $ "could not parse enum value: " <> err
@@ -126,11 +135,17 @@ parseVkBitmaskValueAttrs :: ReaderT ParseLoc AttrParser VkBitmaskValue
 parseVkBitmaskValueAttrs = do
   n <- VkEnumValueName <$> forceAttr "name"
   mc <- lift $ attr "comment"
-  ev <- T.decimal <$> forceAttr "bitpos"
-  case ev of
-    Left err -> parseFailed
-              $ "could not parse bitmask bitpos: " <> err
-    Right (i, _) -> pure $ VkBitmaskValue n mc i
+  mbpv <- fmap T.decimal <$> lift (attr "bitpos")
+  mvav <- fmap (T.signed T.decimal) <$> lift (attr "value")
+  case (mbpv, mvav) of
+    (Nothing, Nothing) -> parseFailed
+                          "missing bitpos or value attribute for a bitmask."
+    (Just (Right (i, _)), _) -> pure $ VkBitmaskBitpos n mc i
+    (_, Just (Right (i, _))) -> pure $ VkBitmaskValue n mc i
+    (Just (Left err), _) -> parseFailed
+                          $ "could not parse bitmask bitpos: " <> err
+    (_, Just (Left err)) -> parseFailed
+                          $ "could not parse bitmask value: " <> err
 
 
 parseVkConstantAttrs :: ReaderT ParseLoc AttrParser VkConstant
