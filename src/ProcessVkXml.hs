992a55e6becc246d161bdae515129ab9c81f1222
achirkin <chirkin@arch.ethz.ch>
2018-01-06 19:59:11 +0100

    Parsing vk.xml version 1 done!

--- src/ProcessVkXml.hs
+++ src/ProcessVkXml.hs
@@ -4,23 +4,35 @@
 {-# LANGUAGE OverloadedStrings     #-}
 {-# LANGUAGE Rank2Types            #-}
 {-# LANGUAGE Strict                #-}
-module ProcessVkXml (processVkXmlFile, generateVkSource) where
+{-# LANGUAGE LambdaCase            #-}
+module ProcessVkXml
+  ( processVkXmlFile
+  , generateVkSource
+  , VkXml (..), InOrder (..)
+  ) where
 
 import           Control.Monad                (unless)
 import           Control.Monad.Catch
 import           Control.Monad.Reader
 import           Control.Monad.Trans.Resource
+import           Control.Monad.State.Class
 import           Data.Conduit
+import           Data.Conduit.Lift
 import           Data.Conduit.Binary          (sourceFile)
 import           Data.Semigroup
 import           Data.XML.Types
+import           Data.Text (Text)
+import           Data.Foldable (toList)
+import           Data.Sequence (Seq, (|>))
+import qualified Data.Sequence as Seq
+-- import           Data.IntMap.Strict (IntMap)
+-- import qualified Data.IntMap.Strict as IntMap
 import           Debug.Trace
 import           Path
 import           Path.IO
 import           Text.XML                     as Xml
 import           Text.XML.Stream.Parse        as Xml
 
-import           VkXml.CommonTypes
 import           VkXml.Parser
 import           VkXml.Sections.Commands
 import           VkXml.Sections.Enums
@@ -57,71 +69,121 @@ generateVkSource ::
        VkXmlParser m
     => Path b Dir
     -> Sink Event m ()
-generateVkSource outputDir = do
-    mev <- await
-    case mev of
-      Nothing -> return ()
-      Just ev -> do
-        f ev
-        generateVkSource outputDir
+generateVkSource _outputDir = do
+    xmlP <- execStateC (VkXmlPartial mempty mempty mempty mempty
+                                     mempty mempty mempty mempty 0)
+      $ tagIgnoreAttrs "registry" parseAll
+    traceShowM $ fixVkXml xmlP
+  where
+    parseAll = do
+      mr <- choose
+        [ tagIgnoreAttrs "comment" $ do
+            com <- content
+            modify' $ \v -> v
+              { gpComments = gpComments v |> InOrder (gpCurLength v) com
+              , gpCurLength = gpCurLength v + 1
+              }
+        , parseVendorIds >>= \case
+            Nothing -> pure Nothing
+            Just x  -> fmap (const $ Just ()) . modify' $ \v -> v
+                { gpVendorIds = gpVendorIds v |> InOrder (gpCurLength v) x
+                , gpCurLength = gpCurLength v + 1
+                }
+        , parseTags >>= \case
+            Nothing -> pure Nothing
+            Just x  -> fmap (const $ Just ()) . modify' $ \v -> v
+                { gpTags = gpTags v |> InOrder (gpCurLength v) x
+                , gpCurLength = gpCurLength v + 1
+                }
+        , parseTypes >>= \case
+            Nothing -> pure Nothing
+            Just x  -> fmap (const $ Just ()) . modify' $ \v -> v
+                { gpTypes = gpTypes v |> InOrder (gpCurLength v) x
+                , gpCurLength = gpCurLength v + 1
+                }
+        , parseEnums >>= \case
+            Nothing -> pure Nothing
+            Just x  -> fmap (const $ Just ()) . modify' $ \v -> v
+                { gpEnums = gpEnums v |> InOrder (gpCurLength v) x
+                , gpCurLength = gpCurLength v + 1
+                }
+        , parseCommands >>= \case
+            Nothing -> pure Nothing
+            Just x  -> fmap (const $ Just ()) . modify' $ \v -> v
+                { gpCommands = gpCommands v |> InOrder (gpCurLength v) x
+                , gpCurLength = gpCurLength v + 1
+                }
+        , parseFeature >>= \case
+            Nothing -> pure Nothing
+            Just x  -> fmap (const $ Just ()) . modify' $ \v -> v
+                { gpFeature = gpFeature v |> InOrder (gpCurLength v) x
+                , gpCurLength = gpCurLength v + 1
+                }
+        , parseExtensions >>= \case
+            Nothing -> pure Nothing
+            Just x  -> fmap (const $ Just ()) . modify' $ \v -> v
+                { gpExtensions = gpExtensions v |> InOrder (gpCurLength v) x
+                , gpCurLength = gpCurLength v + 1
+                }
+        ]
+      case mr of
+        Nothing -> return ()
+        Just () -> parseAll
 
-f :: VkXmlParser m => Event -> Sink Event m ()
-f EventBeginDocument                     = return ()
-f EventEndDocument                       = return ()
-f (EventBeginDoctype _text _mexternalID) = return ()
-f EventEndDoctype                        = return ()
-f (EventInstruction _instruction)        = return ()
-f ev@(EventBeginElement "types" _)       = do
-    leftover ev
-    mtypes <- parseTypes
-    traceM $ "Parsed "
-          <> show (maybe 0 (length . items . types) mtypes) <> " types."
-f ev@(EventBeginElement "enums" _)       = do
-    leftover ev
-    menums <- parseEnums
-    mapM_ (traceM . reportEnums) menums
-f ev@(EventBeginElement "commands" _)       = do
-    leftover ev
-    mcoms <- parseCommands
-    -- mapM_ traceShowM (maybe [] commands mcoms)
-    traceM $ "Parsed "
-          <> show (maybe 0 (length . commands) mcoms) <> " commands."
-f ev@(EventBeginElement "feature" _)       = do
-    leftover ev
-    mfeature <- parseFeature
-    traceM $ "Parsed "
-          <> show (maybe 0 (length . reqList) mfeature)
-          <> " requires in a feature."
-f ev@(EventBeginElement "vendorids" _)       = do
-    leftover ev
-    mvs <- parseVendorIds
-    traceM $ "Parsed "
-          <> show (maybe 0 (length . vendorids) mvs)
-          <> " vendorids."
-f ev@(EventBeginElement "tags" _)       = do
-    leftover ev
-    mtags <- parseTags
-    traceM $ "Parsed "
-          <> show (maybe 0 (length . tags) mtags)
-          <> " tags."
-f ev@(EventBeginElement "extensions" _)       = do
-    leftover ev
-    mextensions <- parseExtensions
-    -- mapM_ traceShowM (maybe [] extensions mextensions)
-    traceM $ "Parsed "
-          <> show (maybe 0 (length . extensions) mextensions)
-          <> " extensions."
-f (EventBeginElement _name _attrs)       = return ()
-f (EventEndElement _name)                = return ()
-f (EventContent _content)                = return ()
-f (EventComment _txt)                    = return ()
-f (EventCDATA _txt)                      = return ()
 
 
-reportEnums :: VkEnums -> String
-reportEnums VkEnums {memberEnums = xs, name = VkTypeName n }
-  = "Parsed " <> show (length . items $ xs) <> " enums " <> show n <> "."
-reportEnums VkBitmasks {memberMasks = xs, name = VkTypeName n }
-  = "Parsed " <> show (length . items $ xs) <> " bitmasks " <> show n <> "."
-reportEnums VkConstants {memberConsts = xs, name = VkTypeName n }
-  = "Parsed " <> show (length . items $ xs) <> " constants " <> show n <> "."
+data InOrder a = InOrder
+  { getOrder :: Int
+  , unInorder :: a
+  } deriving (Eq, Show)
+
+
+data VkXml
+  = VkXml
+  { globComments  :: [InOrder Text]
+  , globVendorIds  :: InOrder VendorIds
+  , globTags       :: InOrder VkTags
+  , globTypes      :: InOrder VkTypes
+  , globEnums      :: [InOrder VkEnums]
+  , globCommands   :: InOrder VkCommands
+  , globFeature    :: InOrder VkFeature
+  , globExtensions :: InOrder VkExtensions
+  } deriving Show
+
+
+data VkXmlPartial
+  = VkXmlPartial
+  { gpComments   :: Seq (InOrder Text)
+  , gpVendorIds  :: Seq (InOrder VendorIds)
+  , gpTags       :: Seq (InOrder VkTags)
+  , gpTypes      :: Seq (InOrder VkTypes)
+  , gpEnums      :: Seq (InOrder VkEnums)
+  , gpCommands   :: Seq (InOrder VkCommands)
+  , gpFeature    :: Seq (InOrder VkFeature)
+  , gpExtensions :: Seq (InOrder VkExtensions)
+  , gpCurLength  :: Int
+  } deriving Show
+
+
+fixVkXml :: VkXmlPartial
+         -> VkXml
+fixVkXml VkXmlPartial
+  { gpComments   = pComments
+  , gpVendorIds  = Seq.Empty Seq.:|> pVendorIds
+  , gpTags       = Seq.Empty Seq.:|> pTags
+  , gpTypes      = Seq.Empty Seq.:|> pTypes
+  , gpEnums      = pEnums
+  , gpCommands   = Seq.Empty Seq.:|> pCommands
+  , gpFeature    = Seq.Empty Seq.:|> pFeature
+  , gpExtensions = Seq.Empty Seq.:|> pExtensions
+  } = VkXml
+  { globComments   = toList pComments
+  , globVendorIds  = pVendorIds
+  , globTags       = pTags
+  , globTypes      = pTypes
+  , globEnums      = toList pEnums
+  , globCommands   = pCommands
+  , globFeature    = pFeature
+  , globExtensions = pExtensions
+  }
+fixVkXml _ = error "Unexpected number of sections in vk.xml"
