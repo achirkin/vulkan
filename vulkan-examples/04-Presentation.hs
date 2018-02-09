6332cc7c5332ddf9c6ccefd2d4320817f8bcd805
achirkin <chirkin@arch.ethz.ch>
2018-02-09 17:16:45 +0100

    Created presentation queue

--- vulkan-examples/04-Presentation.hs
+++ vulkan-examples/04-Presentation.hs
@@ -17,21 +17,23 @@ import           Foreign.Storable
 import           Graphics.Vulkan
 import qualified Graphics.UI.GLFW      as GLFW
 import           Graphics.Vulkan.Ext.VK_KHR_surface
+import qualified Data.Map as Map
 
 import           Lib.GLFW
 import           Lib.Utils
 import           Lib.Vulkan
+
 
 main :: IO ()
 main = withGLFWWindow 800 600 "03-LogicalDev-Window" $ \window ->
        withGLFWVulkanInstance "03-LogicalDevice" $ \vulkanInstance ->
        withSurface vulkanInstance window $ \vulkanSurface -> do
         pdev <- pickPhysicalDevice vulkanInstance
-        withGraphicsDevice pdev $ \dev queue -> do
+        withGraphicsDevice pdev vulkanSurface $ \dev queues -> do
           putStrLn $ "Selected physical device: " ++ show pdev
           putStrLn $ "Createad surface: " ++ show vulkanSurface
           putStrLn $ "Createad device: " ++ show dev
-          putStrLn $ "Createad queue: " ++ show queue
+          putStrLn $ "Createad queues: " ++ show queues
           glfwMainLoop window (return ())
 
 
@@ -62,30 +64,61 @@ getQueueFamilies pdev = alloca $ \qFamCountPtr -> do
 
 -- | Throw an error otherwise
 selectGraphicsFamily :: [(Word32, VkQueueFamilyProperties)]
-                     -> (Word32, VkQueueFamilyProperties)
+                     -> IO (Word32, VkQueueFamilyProperties)
 selectGraphicsFamily []
-  = throw $ VulkanException Nothing "selectGraphicsFamily: not found!"
+  = throwVKMsg "selectGraphicsFamily: not found!"
 selectGraphicsFamily (x@(_,qfp):xs)
   = if  getField @"queueCount" qfp > 0
      && getField @"queueFlags" qfp .&. VK_QUEUE_GRAPHICS_BIT /= zeroBits
-    then x
+    then pure x
     else selectGraphicsFamily xs
 
 
+-- | Throw an error otherwise
+selectPresentationFamily :: VkPhysicalDevice
+                         -> VkSurfaceKHR
+                         -> [(Word32, VkQueueFamilyProperties)]
+                         -> IO (Word32, VkQueueFamilyProperties)
+selectPresentationFamily _ _ []
+  = throw $ VulkanException Nothing "selectGraphicsFamily: not found!"
+selectPresentationFamily dev surf (x@(i,qfp):xs)
+  | getField @"queueCount" qfp <= 0 = selectGraphicsFamily xs
+  | otherwise = do
+    supported <- alloca $ \supportedPtr -> do
+      throwingVK
+        "vkGetPhysicalDeviceSurfaceSupportKHR: failed to check for presentation support."
+        $ vkGetPhysicalDeviceSurfaceSupportKHR dev i surf supportedPtr
+      peek supportedPtr
+    if supported == VK_TRUE
+    then pure x
+    else selectPresentationFamily dev surf xs
+
+
+data DevQueues
+  = DevQueues
+  { _graphicsQueue :: VkQueue
+  , _presentQueue  :: VkQueue
+  } deriving (Eq, Show)
+
+
 withGraphicsDevice :: VkPhysicalDevice
-                   -> (VkDevice -> VkQueue -> IO ()) -> IO ()
-withGraphicsDevice pdev action
+                   -> VkSurfaceKHR
+                   -> (VkDevice -> DevQueues -> IO ()) -> IO ()
+withGraphicsDevice pdev surf action
   | layers <- ["VK_LAYER_LUNARG_standard_validation"]
   =
   alloca $ \queuePrioritiesPtr ->
   withCStringList layers $ \layerCount layerNames -> do
 
   -- find an appropriate queue family
-  (gFamIdx, _gFam) <- selectGraphicsFamily <$> getQueueFamilies pdev
+  qfams <- getQueueFamilies pdev
+  (gFamIdx, _gFam) <- selectGraphicsFamily qfams
+  (pFamIdx, _pFam) <- selectPresentationFamily pdev surf qfams
+  let qFamIndices = Map.fromList [(gFamIdx, gFamIdx), (pFamIdx, pFamIdx)]
 
 
-  qcInfo <- newVkData @VkDeviceQueueCreateInfo
-                      $ \qcInfoPtr -> do
+  qcInfoMap <- forM qFamIndices $ \qFamIdx ->
+               newVkData @VkDeviceQueueCreateInfo $ \qcInfoPtr -> do
     writeField @"sType"
       qcInfoPtr VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO
     writeField @"pNext"
@@ -93,12 +126,14 @@ withGraphicsDevice pdev action
     writeField @"flags"
       qcInfoPtr 0
     writeField @"queueFamilyIndex"
-      qcInfoPtr gFamIdx
+      qcInfoPtr qFamIdx
     writeField @"queueCount"
       qcInfoPtr 1
     poke queuePrioritiesPtr 1.0
     writeField @"pQueuePriorities"
       qcInfoPtr queuePrioritiesPtr
+  -- all qcInfos are copied, no need to touch qcInfoMap anymore
+  qcInfosPts <- newArray $ Map.elems qcInfoMap
 
   pdevFeatures <- newVkData @VkPhysicalDeviceFeatures clearStorable
 
@@ -111,9 +146,9 @@ withGraphicsDevice pdev action
     writeField @"flags"
       devCreateInfoPtr 0
     writeField @"pQueueCreateInfos"
-      devCreateInfoPtr (unsafePtr qcInfo)
+      devCreateInfoPtr qcInfosPts
     writeField @"queueCreateInfoCount"
-      devCreateInfoPtr 1
+      devCreateInfoPtr (fromIntegral $ Map.size qcInfoMap)
     writeField @"enabledLayerCount"
       devCreateInfoPtr (fromIntegral layerCount)
     writeField @"ppEnabledLayerNames"
@@ -132,13 +167,19 @@ withGraphicsDevice pdev action
                        VK_NULL_HANDLE devPtr
     peek devPtr
 
-  -- get the first and the only requested queue
-  gQueue <- alloca $ \quPtr -> do
-     vkGetDeviceQueue dev gFamIdx 0 quPtr
+  -- get the queues
+  gQueues <- flip Map.traverseWithKey qcInfoMap $ \qFamIdx _ ->
+             alloca $ \quPtr -> do
+     vkGetDeviceQueue dev qFamIdx 0 quPtr
      peek quPtr
+  let mdevQueues = DevQueues
+               <$> Map.lookup gFamIdx gQueues
+               <*> Map.lookup pFamIdx gQueues
 
-  finally (action dev gQueue) $ do
+  finally ( maybe (throwVKMsg "Some queues lost!") pure mdevQueues
+           >>= action dev
+          ) $ do
     vkDestroyDevice dev VK_NULL_HANDLE
     touchVkData devCreateInfo
     touchVkData pdevFeatures
-    touchVkData qcInfo
+    free qcInfosPts
