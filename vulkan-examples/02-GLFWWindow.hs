48af53327a5dde74c90ba9fc2fe99aec6f3c8b91
achirkin <chirkin@arch.ethz.ch>
2018-02-09 11:30:57 +0100

    reworked examples a bit

--- vulkan-examples/02-GLFWWindow.hs
+++ vulkan-examples/02-GLFWWindow.hs
@@ -1,9 +1,14 @@
+{- |
+
+In this example, I follow first parts of vulkan-tutorial.com to create a window
+and pick a device using GLFW.
+
+Parts of the code are moved to Lib.hs
+-}
 module Main (main) where
 
 import           Control.Monad
-import           Control.Monad.Trans.Class
-import           Control.Monad.Trans.Except
-import           Foreign.C.String
+import           Control.Exception
 import           Foreign.Marshal.Alloc
 import           Foreign.Marshal.Array
 import           Foreign.Storable
@@ -14,93 +19,80 @@ import           Graphics.Vulkan
 import           Lib
 
 main :: IO ()
-main = do
-    successfulInit <- GLFW.init
-    if not successfulInit
-    then error "Failed to init GLFW"
-    else do
-      GLFW.getVersionString >>= mapM_ (putStrLn . ("GLFW version: " ++))
-      vkSupport <- GLFW.vulkanSupported
-      if not vkSupport
-      then putStrLn "GLFW says Vulkan is not supported!"
-      else do
-        GLFW.windowHint $ WindowHint'ClientAPI ClientAPI'NoAPI
-        GLFW.windowHint $ WindowHint'Resizable False
-        mw <- GLFW.createWindow 800 600 "Vulkan window" Nothing Nothing
-        case mw of
-          Nothing -> putStrLn "Failed to initialize GLFW window"
-          Just w  -> do
-            vkRes <- withVulkanInstanceExt $ \vulkanInstance -> do
-              edev <- pickPhysicalDevice vulkanInstance
-              case edev of
-                Left err -> putStrLn err
-                Right dev -> do
-                  print dev
-                  mainLoop w
-              return VK_SUCCESS
-            print vkRes
-      putStrLn "Done."
-      GLFW.terminate
-      putStrLn "Terminated GLFW."
+main = withGLFWWindow $ \window ->
+       withVulkanInstanceExt $ \vulkanInstance -> do
+          dev <- pickPhysicalDevice vulkanInstance
+          putStrLn $ "Selected device: " ++ show dev
+          glfwMainLoop window (return ())
+
+
+withGLFWWindow :: (GLFW.Window -> IO ()) -> IO ()
+withGLFWWindow action = do
+  GLFW.init >>= flip unless
+    (throwVKMsg "Failed to initialize GLFW.")
+
+  -- even if something bad happens, we need to terminate GLFW
+  flip finally (GLFW.terminate >> putStrLn "Terminated GLFW.") $ do
+    GLFW.getVersionString >>= mapM_ (putStrLn . ("GLFW version: " ++))
+
+    GLFW.vulkanSupported >>= flip unless
+      (throwVKMsg "GLFW reports that vulkan is not supported!")
+
+    GLFW.windowHint $ WindowHint'ClientAPI ClientAPI'NoAPI
+    GLFW.windowHint $ WindowHint'Resizable False
+
+    mw <- GLFW.createWindow 800 600 "Vulkan window" Nothing Nothing
+    case mw of
+      Nothing -> throwVKMsg "Failed to initialize GLFW window."
+      Just w  -> do
+        putStrLn "Initialized GLFW window."
+        finally (action w)
+                (GLFW.destroyWindow w >> putStrLn "Closed GLFW window.")
+
+
+glfwMainLoop :: GLFW.Window -> IO () -> IO ()
+glfwMainLoop w action = go
   where
-    mainLoop w = do
-      shouldClose <- GLFW.windowShouldClose w
-      if shouldClose
-      then do
-        putStrLn "Closing the window..."
-        GLFW.destroyWindow w
-      else do
-        GLFW.pollEvents
-        mainLoop w
+    go = do
+      should <- GLFW.windowShouldClose w
+      unless should $ GLFW.pollEvents >> action >> go
 
 
-withVulkanInstanceExt :: (VkInstance -> IO VkResult) -> IO VkResult
+
+
+withVulkanInstanceExt :: (VkInstance -> IO ()) -> IO ()
 withVulkanInstanceExt action = do
     -- get required extension names from GLFW
-    (reLength, reqExts) <- GLFW.getRequiredInstanceExtensions
-    -- get required layers
-    withCString "VK_LAYER_LUNARG_standard_validation" $ \stdValidationLayer ->
-      let vlList = [ stdValidationLayer
-                   ]
-      in withArray vlList $ \validationLayersPtr ->
-          withVulkanInstance "02-GLFWWindow"
-                             (fromIntegral reLength, reqExts)
-                             (length vlList, validationLayersPtr)
-                             action
+    glfwReqExts <- GLFW.getRequiredInstanceExtensions
+               >>= uncurry (peekArray . fromIntegral)
+    withVulkanInstance
+      "02-GLFWWindow"
+      glfwReqExts
+      ["VK_LAYER_LUNARG_standard_validation"]
+      action
 
 
 
-pickPhysicalDevice :: VkInstance -> IO (Either String VkPhysicalDevice)
-pickPhysicalDevice vkInstance = runExceptT $ do
-    devs <- ExceptT $ alloca $ \deviceCountPtr -> runExceptT $ do
-      throwingVK "pickPhysicalDevice: Failed to enumerate physical devices"
-                $ vkEnumeratePhysicalDevices vkInstance deviceCountPtr VK_NULL_HANDLE
-      devCount <- lift $ fromIntegral <$> peek deviceCountPtr
-      when (devCount <= 0) $ throwE "Zero device count!"
-      devices <- ExceptT $ allocaArray devCount $ \devicesPtr -> runExceptT $ do
-        throwingVK "pickPhysicalDevice: Failed to enumerate physical devices"
-                $ vkEnumeratePhysicalDevices vkInstance deviceCountPtr devicesPtr
-        lift $ peekArray devCount devicesPtr
-      lift $ putStrLn $ "pickPhysicalDevice: Found " ++ show devCount ++ " devices."
-      return devices
+pickPhysicalDevice :: VkInstance -> IO VkPhysicalDevice
+pickPhysicalDevice vkInstance = do
+    devs <- alloca $ \deviceCountPtr -> do
+      throwingVK "pickPhysicalDevice: Failed to enumerate physical devices."
+        $ vkEnumeratePhysicalDevices vkInstance deviceCountPtr VK_NULL_HANDLE
+      devCount <- fromIntegral <$> peek deviceCountPtr
+      when (devCount <= 0) $ throwVKMsg "Zero device count!"
+      putStrLn $ "Found " ++ show devCount ++ " devices."
+
+      allocaArray devCount $ \devicesPtr -> do
+        throwingVK "pickPhysicalDevice: Failed to enumerate physical devices."
+          $ vkEnumeratePhysicalDevices vkInstance deviceCountPtr devicesPtr
+        peekArray devCount devicesPtr
 
     selectFirstSuitable devs
   where
-    selectFirstSuitable [] = throwE "No suitable devices!"
-    selectFirstSuitable (x:xs) = lift (isDeviceSuitable x) >>= \yes ->
+    selectFirstSuitable [] = throwVKMsg "No suitable devices!"
+    selectFirstSuitable (x:xs) = isDeviceSuitable x >>= \yes ->
       if yes then pure x
              else selectFirstSuitable xs
 
 isDeviceSuitable :: VkPhysicalDevice -> IO Bool
 isDeviceSuitable _ = pure True
-
-
-
-
-throwingVK :: String
-           -> IO VkResult
-           -> ExceptT String IO ()
-throwingVK msg f = do
-  vkRez <- lift f
-  unless (vkRez == VK_SUCCESS || vkRez == VK_INCOMPLETE) $
-    throwE $ msg ++ " " ++ show vkRez
