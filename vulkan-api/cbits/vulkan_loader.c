6fda7456e7ff79eb9071fda7240bff13030d9f95
achirkin <hirkin@arch.ethz.ch>
2018-04-13 13:47:06 +0200

    Dynamic loading on Windows

--- vulkan-api/cbits/vulkan_loader.c
+++ vulkan-api/cbits/vulkan_loader.c
@@ -1,9 +1,30 @@
 #include "vulkan_loader.h"
 
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) || defined(mingw32_HOST_OS)
+  #include <windows.h>
+  #define _vk_dlopen() LoadLibraryA("vulkan-1.dll")
+  #define _vk_dlclose(handle) FreeLibrary((HMODULE) handle)
+  #define _vk_dlsym(handle, name) GetProcAddress((HMODULE) handle, name)
+  #define _vk_dlerror(msg) *msg = errMsgBuf; FormatMessage( \
+        FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, \
+        NULL, GetLastError(), MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPTSTR) errMsgBuf , 255, NULL )
+  static TCHAR errMsgBuf[255] = { 0 };
+#else
+  #include <dlfcn.h>
+  #if defined(__APPLE__) || defined(darwin_HOST_OS)
+    #define _vk_dlopen() dlopen("libMoltenVK.dylib", RTLD_LAZY | RTLD_LOCAL)
+  #else
+    #define _vk_dlopen() dlopen("libvulkan.so", RTLD_LAZY | RTLD_LOCAL)
+  #endif
+  #define _vk_dlclose(handle) dlclose(handle)
+  #define _vk_dlsym(handle, name) dlsym(handle, name)
+  #define _vk_dlerror(msg) *msg = dlerror()
+#endif
+
 
 void *_vkdll_dlinit(char **errorMsg){
   void *handle = (void*)_vk_dlopen();
-  *errorMsg = dlerror();
+  _vk_dlerror(errorMsg);
   return handle;
 }
 
@@ -13,6 +34,6 @@ void _vkdll_dlclose(void *handle){
 
 void *_vkdll_dlsym(void *handle, const char *symbol, char **errorMsg){
   void *rez = (void*)_vk_dlsym(handle, symbol);
-  *errorMsg = dlerror();
+  _vk_dlerror(errorMsg);
   return rez;
 }
--- vulkan-api/include/vulkan_loader.h
+++ vulkan-api/include/vulkan_loader.h
@@ -1,23 +1,6 @@
 #ifndef VULKAN_LOADER_H_
 #define VULKAN_LOADER_H_ 1
 
-#if mingw32_HOST_OS
-  #include <WinBase.h>
-  #define _vk_dlopen() LoadLibraryA("vulkan-1.dll")
-  #define _vk_dlclose(handle) FreeLibrary((HMODULE) handle)
-  #define _vk_dlsym(handle, name) GetProcAddress((HMODULE) handle, name)
-#else
-  #include <dlfcn.h>
-  #ifdef darwin_HOST_OS
-    #define _vk_dlopen() dlopen("libMoltenVK.dylib", RTLD_LAZY | RTLD_LOCAL)
-  #else
-    #define _vk_dlopen() dlopen("libvulkan.so", RTLD_LAZY | RTLD_LOCAL)
-  #endif
-  #define _vk_dlclose(handle) dlclose(handle)
-  #define _vk_dlsym(handle, name) dlsym(handle, name)
-#endif
-
-
 void *_vkdll_dlinit(char **errorMsg);
 void  _vkdll_dlclose(void *handle);
 void *_vkdll_dlsym(void *handle, const char *symbol, char **errorMsg);
