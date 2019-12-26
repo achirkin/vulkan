38d3f023bb7f662ff3be843e144fa7232abdd3f7
o1lo01ol1o <tim.pierson@gmail.com>
2019-12-26 14:07:12 +0000

    make it work

--- default.nix
+++ default.nix
@@ -5,20 +5,20 @@ let
     url = "${spec.url}/archive/${spec.rev}.tar.gz";
     inherit (spec) sha256;
   });
-in { pkgs' ? pkgs, compiler ? "ghc865" }:
+in { pkgs' ? pkgs, compiler ? "ghc864" }:
 let
 
   pkgs = import pkgs' { };
   # TODO: vulkan needs to be installed and passed to all the subpackages with the platform appropriate frameworks.
-  default = {
-    vulkan-examples =
-      pkgs.haskellPackages.callCabal2nix "vulkan-examples" ./vulkan-examples
-      { };
-    vulkan-triangles =
-      pkgs.haskellPackages.callCabal2nix "vulkan-triangles" ../vulkan-triangles
-      { };
-    vulkan-api =
-      pkgs.haskellPackages.callCabal2nix "vulkan-api" ../vulkan-api { };
-  };
+  # default = {
+  #   vulkan-examples =
+  #     pkgs.haskellPackages.callCabal2nix "vulkan-examples" ./vulkan-examples
+  #     { };
+  #   vulkan-triangles =
+  #     pkgs.haskellPackages.callCabal2nix "vulkan-triangles" ../vulkan-triangles
+  #     { };
+  #   vulkan-api =
+  #     pkgs.haskellPackages.callCabal2nix "vulkan-api" ../vulkan-api { };
+  # };
   darwin = import ./nix/default-darwin.nix { inherit pkgs' compiler; };
-in if pkgs.stdenv.isDarwin then darwin else default
+in darwin#  if pkgs.stdenv.isDarwin then darwin else default
